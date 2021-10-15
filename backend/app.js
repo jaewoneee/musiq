e6bbcfbc4bd58936857454b0c791ef407f676018
jaewoneee <jaewon810@naver.com>
2021-10-15 11:24:31 +0900

    JWT 이용하여 토큰값 생성하기

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,32 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var cors = require("cors");
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 var usersRouter = require("./routes/users");
 var listRouter = require("./routes/playlist");
 var musicRouter = require("./routes/music");
+
+// Utils
+var auth = require('./utils/auth');
+
 var app = express();
+
+// Session
+app.use(session({
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  store : new MySQLStore({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'qlrqod12',
+    database:'vue_db'
+  })
+}));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -25,7 +46,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use('/api/users', usersRouter);
 app.use('/api/main', listRouter);
-app.use("/api/search", musicRouter);
+app.use("/api/search", auth.authUser, musicRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
